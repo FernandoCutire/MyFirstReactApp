819feb8bb3cc543d7d7e4968fadbc8b8fbaa1a5c
Fernando Cutire UTP <fernando.cutire@utp.ac.pa>
2020-02-05 03:05:37 +0000

    Ejercicios añadidos

---
+++ src/Componentes/Ejercicio.js
@@ -0,0 +1,26 @@
+/*
+----------------------------------------------------------------
+                    React: Ejercicio 1
+    Escribe un componente que muestre sus props y estado
+    en consola.
+    Además, usa los métodos del ciclo de vida de React
+    y muestra mediante console log cuándo ocurren éstos. 
+----------------------------------------------------------------
+*/
+/*
+----------------------------------------------------------------
+                    React: Ejercicio 2
+    Implementa un componente que reciba una lista de frutas
+    mediante props y las despliegue en pantalla
+----------------------------------------------------------------
+*/
+/*
+----------------------------------------------------------------
+                    React: Ejercicio 3
+    Crea un componente que tenga un text input que muestre
+    debajo, con un contador, cuántas letras contiene el input.
+    Si el input contiene 0 letras, el contador debe mostrar 
+    'cero'. Si el input contiene una o más letras, el contador
+    debe mostrar el número del conteo.
+----------------------------------------------------------------
+*/
